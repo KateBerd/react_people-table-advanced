806ac1aeb6e3c44a4ecb253ae5b190751637104d
Kateryna Berdnikova <berdekaterinasp@gmail.com>
2025-11-03 12:53:36 +0200

    solution3

--- src/pages/PeoplePage.tsx
+++ src/pages/PeoplePage.tsx
@@ -5,6 +5,8 @@ import { getPeople } from '../api';
 import { PeopleTable } from '../components/PeopleTable';
 import { PeopleFilters } from '../components/PeopleFilters';
 import { useSearchParams } from 'react-router-dom';
+import { getFilteredPeople } from '../utils/filterPeople';
+import { getSortedPeople } from '../utils/sortPeople';
 
 export const PeoplePage = () => {
   const [people, setPeople] = useState<Person[]>([]);
@@ -33,77 +35,11 @@ export const PeoplePage = () => {
   }, []);
 
   useEffect(() => {
-    let filteredPeople = [...allPeople];
+    const filteredPeople = getFilteredPeople(allPeople, sex, query, centuries);
+    const sortedPeople = getSortedPeople(filteredPeople, sort, order);
 
-    if (sex) {
-      filteredPeople = filteredPeople.filter(person => person.sex === sex);
-    }
-
-    if (query) {
-      filteredPeople = filteredPeople.filter(
-        person =>
-          person.name.includes(query) ||
-          person.motherName?.includes(query) ||
-          person.fatherName?.includes(query),
-      );
-    }
-
-    if (centuries.length) {
-      filteredPeople = filteredPeople.filter(person =>
-        centuries.includes(`${Math.floor((person.born - 1) / 100) + 1}`),
-      );
-    }
-
-    if (sort === 'name') {
-      filteredPeople = filteredPeople.sort((person1, person2) =>
-        person1.name.localeCompare(person2.name),
-      );
-    }
-
-    if (sort === 'name' && order) {
-      filteredPeople = filteredPeople.sort((person1, person2) =>
-        person2.name.localeCompare(person1.name),
-      );
-    }
-
-    if (sort === 'sex') {
-      filteredPeople = filteredPeople.sort((person1, person2) =>
-        person1.sex.localeCompare(person2.sex),
-      );
-    }
-
-    if (sort === 'sex' && order) {
-      filteredPeople = filteredPeople.sort((person1, person2) =>
-        person2.sex.localeCompare(person1.sex),
-      );
-    }
-
-    if (sort === 'born') {
-      filteredPeople = filteredPeople.sort(
-        (person1, person2) => person1.born - person2.born,
-      );
-    }
-
-    if (sort === 'born' && order) {
-      filteredPeople = filteredPeople.sort(
-        (person1, person2) => person2.born - person1.born,
-      );
-    }
-
-    if (sort === 'died') {
-      filteredPeople = filteredPeople.sort(
-        (person1, person2) => person1.died - person2.died,
-      );
-    }
-
-    if (sort === 'died' && order) {
-      filteredPeople = filteredPeople.sort(
-        (person1, person2) => person2.died - person1.died,
-      );
-    }
-
-    setPeople(filteredPeople);
-  }, [sex, query, centuries, sort, order, allPeople]);
+    setPeople(sortedPeople);
+  }, [allPeople, sex, query, centuries, sort, order]);
 
   return (
     <div className="container">
@@ -143,33 +79,3 @@ export const PeoplePage = () => {
     </div>
   );
 };
-
-// export const PeoplePage = () => {
-//   return (
-//     <>
-//       <h1 className="title">People Page</h1>
-
-//       <div className="block">
-//         <div className="columns is-desktop is-flex-direction-row-reverse">
-//           <div className="column is-7-tablet is-narrow-desktop">
-//             <PeopleFilters />
-//           </div>
-
-//           <div className="column">
-//             <div className="box table-container">
-//               <Loader />
-
-//               <p data-cy="peopleLoadingError">Something went wrong</p>
-
-//               <p data-cy="noPeopleMessage">There are no people on the server</p>
-
-//               <p>There are no people matching the current search criteria</p>
-
-//               <PeopleTable />
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
